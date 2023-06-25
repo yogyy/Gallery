1ae982624d523505058789b92abeb213328d3412
yogyy <yogimr261@gmail.com>
2023-06-25 23:12:43 +0700

    add react marquee

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,31 @@
+import Marquee from 'react-fast-marquee';
+import { UnsplashImage } from './models/unsplash-image';
+import Image from 'next/image';
+import { Suspense } from 'react';
+
 export default async function Home() {
+  // const res = await fetch(
+  //   `https://api.unsplash.com/search/photos?query=sea&client_id=${process.env.UNSPLASH_KEY}`
+  // );
+  // const images: UnsplashImage[] = await res.json();
   return (
-    <div className="flex flex-col w-full text-white ">
-      <div className="relative flex items-center justify-center border-red-600">
-        <p>hello</p>
+    <div className="flex flex-col items-center w-full h-screen text-white">
+      <div className="relative flex flex-col items-center justify-around h-full">
+        <h1 className="font-serif text-2xl font-semibold">Welcome to Gallery!!</h1>
+        <Marquee speed={30}>
+          <div className="relative flex items-center ">
+            {[...Array(2)].map((_, index) => (
+              <Image
+                key={index}
+                src="/images/marquee.webp"
+                width={1400}
+                height={500}
+                alt="marquee"
+                className="object-scale-down h-auto"
+              />
+            ))}
+          </div>
+        </Marquee>
       </div>
     </div>
   );
